82c274bae2dd882d8c4306d06df441d96e8b640e
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-29 01:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function createRock(x) {
   }
     
   // We should kick off the animation of the rock around here
-  
+  moveRock();  
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
