e549fa897fd2211f39b8003a99458bc8ba2dde8a
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-29 01:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function moveDodger(e) {
 
 function moveDodgerLeft() {
     window.requestAnimationFrame(function(){
-     const left = DODGER.style.left;
+     const left = positionToInteger(DODGER.style.left);
      if(left > 0){
        DODGER.style.left = `${left - 4}px`;
      }
