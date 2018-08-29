109001f3976ec3eef753deba0d746a14540c1c48
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-29 00:21:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function checkCollision(rock) {
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
-  if (rockTop > 300) {
+  if (rockTop > 400) {
     if ((rockLeftEdge > dodgerLeftEdge) && (rockRightEdge < dodgerRightEdge)) {
       return true;
     } 
